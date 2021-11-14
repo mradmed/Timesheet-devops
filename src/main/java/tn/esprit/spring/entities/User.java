1d816829b6372388cba75795265974a15a59426b
Mrad Med <mohamedrached.mrad@esprit.tn>
2021-11-14 13:29:58 +0100

    Testing changes in repo

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -19,7 +19,7 @@ import javax.persistence.TemporalType;
 public class User implements Serializable {
 	
 	//Testing update on git the second time now
-	//Testing again in github
+	//Testing again in github again
 	
 	private static final long serialVersionUID = 1L;
 
