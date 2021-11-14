13f6a20763424d5f6bf0b2279150e450a7cccecf
Mrad Med <mohamedrached.mrad@esprit.tn>
2021-11-14 13:22:29 +0100

    commit in local git

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -19,6 +19,7 @@ import javax.persistence.TemporalType;
 public class User implements Serializable {
 	
 	//Testing update on git the second time now
+	//Testing again 
 	
 	private static final long serialVersionUID = 1L;
 
